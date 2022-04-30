c218feeb5da538ccf2b70f9e4415bd51f81f7b1b
DESKTOP-ELGSLMU\Fukushiro <jpflc2301@gmail.com>
2022-04-30 17:27:59 -0300

    primeiro commit

---
+++ migrations/20220430202027-user_example.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable("user_example", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: { type: Sequelize.STRING, allowNull: false },
+      password: { type: Sequelize.STRING(500), allowNull: false },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.dropTable("user_example");
+  },
+};
